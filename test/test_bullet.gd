d756b5f75d5e4f68dd34a8175d26dc10fc51c518
Egor Kostan <igorkostan@gmail.com>
2025-10-12 20:03:41 -0700

    Update test_bullet.gd

--- test/test_bullet.gd
+++ test/test_bullet.gd
@@ -3,23 +3,30 @@ extends GdUnitTestSuite
 var bullet_scene: = preload("res://scenes/bullet.tscn")
 
 func test_bullet_collision() -> void:
-	var bullet: = bullet_scene.instantiate()
-	get_tree().current_scene.add_child(bullet)
+	# New: auto_free for cleanup (prevents leaks/orphans)
+	var bullet: Variant = auto_free(bullet_scene.instantiate())
+	# Updated: Use root (reliable in CI/tests)
+	get_tree().root.add_child(bullet)
 	bullet.global_position = Vector2.ZERO
 	bullet.global_rotation = 0
-	await get_tree().physics_frame
+	# New: Safer await for tree settling (physics_frame can be flaky in CI)
+	await await_idle_frame()
+	
 	# Simulate a hit body with take_damage method
-	var dummy: Node2D = Node2D.new()
+	# New: auto_free for dummy cleanup
+	var dummy: Node2D = auto_free(Node2D.new())
 	var script: = GDScript.new()
 	script.source_code = """
 extends Node2D
 
 func take_damage(d: int) -> void:
     pass
-	"""
+    """
 	script.reload()
 	dummy.set_script(script)
+	
+	# Emit signal to simulate hit
 	bullet.get_node("Area2D").area_entered.emit(dummy)
+	
+	# Assert bullet queued for deletion after hit
 	assert_that(bullet).is_queued_for_deletion()  # Verify despawn on hit
-	dummy.free()
-	bullet.queue_free()
