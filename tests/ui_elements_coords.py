d85c5875c2247bc79d77042df16ae762585167f9
Egor Kostan <igorkostan@gmail.com>
2025-10-16 17:18:17 -0700

    Create ui_elements_coords.py

---
+++ tests/ui_elements_coords.py
@@ -0,0 +1,18 @@
+"""List of coordinates for all UI elements in the game."""
+
+# Define element coordinates relative to canvas top-left
+UI_ELEMENTS = {
+    "start_game_button": {"X": 645, "y": 288},
+    "options_button": {"x": 629, "y": 357},
+    "quit_button": {"x": 637, "y": 430},
+    "difficulty_slider_0.5": {"x": 629, "y": 324},
+    "difficulty_slider_1.3": {"x": 686, "y": 3324},
+    "difficulty_slider_2.0": {"x": 733, "y": 324},
+    "back_button": {"x": 655, "y": 450},
+    "confirm_dialog_cancel_button": {"x": 659, "y": 429},
+    "confirm_dialog_ok_button": {"x": 550, "y": 429},
+    "confirm_dialog_x_button": {"x": 659, "y": 428},
+    "log_level_combo": {"x": 712, "y": 389},
+    "main_menu_button": {"x": 652, "y": 286},
+    "resume_button": {"x": 639, "y": 429},
+}
