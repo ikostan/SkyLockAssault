5b468ef39f96dfb52fa6ef4aa09504b836a50646
Egor Kostan <igorkostan@gmail.com>
2025-10-30 19:36:27 -0700

    Refactor node access in options_menu.gd
    Replaced shorthand node access ($) with get_node() for log level option, difficulty slider, and difficulty label in options_menu.gd to improve clarity and maintainability. Updated index.html to reflect new index.pck file size.

--- scripts/options_menu.gd
+++ scripts/options_menu.gd
@@ -9,14 +9,22 @@ var log_level_display_to_enum := {
 	"NONE": Globals.LogLevel.NONE
 }
 
-@onready
-var log_lvl_option: OptionButton = $Panel/OptionsVBoxContainer/LogLevelHBoxContainer/LogLevelOptionButton
+@onready var log_lvl_option: OptionButton = get_node(
+	"Panel/OptionsVBoxContainer/" +
+	"LogLevelHBoxContainer/" +
+	"LogLevelOptionButton"
+)
 @onready var back_button: Button = $Panel/OptionsVBoxContainer/BackButton
-@onready
-var difficulty_slider: HSlider = $Panel/OptionsVBoxContainer/DifficultyLevelHBoxContainer/DifficultyHSlider
-@onready
-var difficulty_label: Label = $Panel/OptionsVBoxContainer/DifficultyLevelHBoxContainer/DifficultyValueLabel
-
+@onready var difficulty_slider: HSlider = get_node(
+	"Panel/OptionsVBoxContainer/" +
+	"DifficultyLevelHBoxContainer/" +
+	"DifficultyHSlider"
+)
+@onready var difficulty_label: Label = get_node(
+	"Panel/OptionsVBoxContainer/" +
+	"DifficultyLevelHBoxContainer/" +
+	"DifficultyValueLabel"
+)
 
 func _input(event: InputEvent) -> void:  # Add type hints
 	if event is InputEventMouseButton and event.pressed and event.button_index == MOUSE_BUTTON_LEFT:
