cafaa32cf8c4d6c50ff2e6f3a798f0c9eff0d7e2
Egor Kostan <igorkostan@gmail.com>
2026-04-18 19:19:21 -0700

    Unit test updates

--- test/gdunit4/test_helpers.gd
+++ test/gdunit4/test_helpers.gd
@@ -9,7 +9,7 @@ extends RefCounted
 ## Calculates the expected fuel depletion based on the global GameSettingsResource.
 static func calculate_expected_depletion(player_root: Node, difficulty: float) -> float:
 	# NEW: Use Globals.settings.max_speed instead of player_root.MAX_SPEED
-	var normalized_speed: float = player_root.speed["speed"] / Globals.settings.max_speed
+	var normalized_speed: float = player_root.current_speed / Globals.settings.max_speed
 	
 	# NEW: Use Globals.settings.base_consumption_rate instead of player_root.base_fuel_drain
 	return Globals.settings.base_consumption_rate * normalized_speed * difficulty
