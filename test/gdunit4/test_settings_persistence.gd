9503b91e2e4689f616bf345ecf55887a1c26b1e6
Egor Kostan <igorkostan@gmail.com>
2026-04-27 19:58:29 -0700

    Update test_settings_persistence.gd
    Once again, this is the exact same C++ "magic number" error caused by GdUnit4's error monitor catching Godot's internal warning.
    
    In test_settings_persistence.gd, the test is setting up its initial mock file using the plaintext config.save(test_path). When Globals._load_settings() attempts to read that mock file with load_encrypted_pass(), it triggers the error.
    
    To fix it, we simply need to change the initial save() call to save_encrypted_pass() using Globals.save_encryption_pass.

--- test/gdunit4/test_settings_persistence.gd
+++ test/gdunit4/test_settings_persistence.gd
@@ -3,16 +3,19 @@
 # test_settings_persistence.gd (extends GdUnitTestSuite)
 extends GdUnitTestSuite
 
+
 func test_settings_persistence() -> void:
 	## Tests persistence with isolated path.
 	##
 	## :rtype: void
 	var test_path: String = "user://test_settings.cfg"
 	
-	# Setup: Create and save test config
+	# Setup: Create and save test config using encryption
 	var config: ConfigFile = ConfigFile.new()
 	config.set_value("Settings", "difficulty", 1.5)
-	var err: int = config.save(test_path)
+	
+	# FIX: Save using encrypted pass so Globals._load_settings doesn't throw a C++ core error
+	var err: int = config.save_encrypted_pass(test_path, Globals.save_encryption_pass)
 	if err != OK:
 		fail("Failed to save test config: " + str(err))
 	
@@ -25,6 +28,7 @@ func test_settings_persistence() -> void:
 	# Reload to verify save
 	Globals._load_settings(test_path)
 	assert_float(Globals.settings.difficulty).is_equal(2.0)  # Saved and loaded
+
 
 func after_test() -> void:
 	## Cleans test file.
